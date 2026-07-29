08934ba24e88b6f90636891ea4abe2f4839331e7
ChumchonCity <wanchai.samlee@gmail.com>
2026-07-29 09:18:05 +0700

    article detail meta improving

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -59,17 +59,15 @@ exports.articleDetail = async (req, res) => {
             articles,
             meta: {
 
-                title:
-                    "บทความ และสาระความรู้ เกี่ยวกับรถกระเช้าบูมลิฟท์ เอ็กซ์ลิฟท์ | รถกระเช้าไทย",
+                title:article.meta_title || "บทความรถกระเช้าไทย",
 
-                description:
-                    "เรียนรู้และทำความเข้าใจให้มากขึ้นเกี่ยวกับ เครื่องจักร บูมลิฟท์ และเอ็กซ์ลิฟท์",
+                description:article.meta_description,
 
                 image:
-                    "https://www.rodkrachaothai.com/images/default-og.png",
+                    "https://www.rodkrachaothai.com/images/articles/article.featured_image",
 
                 url:
-                    "https://www.rodkrachaothai.com/articles"
+                    "https://www.rodkrachaothai.com/articles/article.slug"
                 }       
         });
         } catch (error) {
